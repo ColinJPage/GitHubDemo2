481e97ed7516fff9c604e2599dc54f273aaa2d4a
Ethan Whiting <ethernets02@gmail.com>
2022-02-16 19:48:38 -0500

    Delete Start Function
    Delete Start Function, it was totally lame

--- GitHubDemo2/Assets/Scripts/TestScript.cs
+++ GitHubDemo2/Assets/Scripts/TestScript.cs
@@ -4,11 +4,7 @@ using UnityEngine;
 
 public class TestScript : MonoBehaviour
 {
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
+
 
     // Update is called once per frame
     void Update()
